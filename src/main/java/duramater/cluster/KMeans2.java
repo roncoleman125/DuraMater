678ea3b1ed796385cab799e54bf9c2031ab36a8b
Ron.Coleman <ronncoleman@gmail.com>
2022-08-25 16:34:35 -0400

    Finished testing the redesigned kmeans.

--- src/main/java/duramater/cluster/KMeans2.java
+++ src/main/java/duramater/cluster/KMeans2.java
@@ -1,5 +1,10 @@
 package duramater.cluster;
 
+import de.unknownreality.dataframe.DataFrame;
+import de.unknownreality.dataframe.csv.CSVReader;
+import de.unknownreality.dataframe.csv.CSVReaderBuilder;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -28,19 +33,21 @@ public class KMeans2 {
 
         long oldHash = -1;
         while(iteration < MAX_ITERATIONS) {
-            data.forEach(candidate -> {
-                assign(candidate);
+            data.forEach(observation -> {
+                assign(observation);
             });
 
             clusters.forEach(cluster -> {
                 crc.update(cluster.members.size());
             });
+
             long newHash = crc.getValue();
             if(newHash == oldHash)
                 break;
             oldHash = newHash;
             crc.reset();
             recenter();
+            iteration++;
         }
 
     }
@@ -61,13 +68,14 @@ public class KMeans2 {
                 double mean = sum / numRows;
                 centroid[colno] = mean;
             }
+            members.clear();
         }
     }
 
     void init() {
         List<Double[]> centroids = data.subList(0,k);
         IntStream.range(0,k).forEach(idx -> {
-            Double[] centroid = centroids.get(k);
+            Double[] centroid = centroids.get(idx);
             List<Double[]> members = new ArrayList<>();
             Cluster2 cluster = new Cluster2(centroid,members);
             clusters.add(cluster);
@@ -85,10 +93,39 @@ public class KMeans2 {
     }
 
     double getDist(Double[] a, Double[] b) {
-        return 0;
+        double dist2 = 0;
+        for(int i=0; i < a.length; i++) {
+            dist2 += (a[i]-b[i])*(a[i]-b[i]);
+        }
+        return dist2;
     }
 
     public static void main(String[] args) {
-        KMeans2 km = new KMeans2(5,null);
+        CSVReader csvReader = CSVReaderBuilder.create()
+                .containsHeader(true)
+                .withSeparator(',')
+                .setColumnType("INCOME", Double.class)
+                .setColumnType("SPEND", Double.class)
+                .build();
+        DataFrame frame = DataFrame.load(new File("data/KMeans Dataset.csv"), csvReader);
+
+        List<Double[]> data = new ArrayList<>();
+
+        frame.rows().forEach(row -> {
+            double income = row.getDouble(0);
+            double spend = row.getDouble(1);
+            double netMoney =  income - spend;
+            Double[] observation = {income,spend,netMoney};
+            data.add(observation);
+        });
+        KMeans2 km = new KMeans2(3,data);
+        km.train();
+        List<Cluster2> clusters = km.getCluster();
+        clusters.forEach(cluster -> {
+            cluster.members.forEach(member -> {
+                System.out.println(member[0]+","+member[1]);
+            });
+            System.out.println("###");
+        });
     }
 }
